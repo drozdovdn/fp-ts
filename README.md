1e0179b5f63bc6e8d73c4469fa5014ea0325278f
drozdov <drozdov@skytracking.ru>
2022-07-05 16:08:16 +0300

    add flow

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ node params for webstorm
 ```
  --require ts-node/register 
 ```
+[1](https://rlee.dev/practical-guide-to-fp-ts-part-1) pipe and flow
+
--- src/1_pipe.ts
+++ src/1_pipe.ts
@@ -1,18 +1,37 @@
-import { pipe } from 'fp-ts/lib/function'
+import { pipe, flow } from 'fp-ts/lib/function'
 
-const add1 = (num:number) => {
+const init = (num: number): number => {
+    return num
+}
+const add1 = (num:number):number => {
     return num + 1
 }
 
-const multiply2 = (num:number) => {
+const multiply2 = (num:number):number => {
     return num * 2
 }
-
+const toString = (num: number):string => {
+    return `${num}`
+}
 const out = pipe(
-    1,
-    add1,
-    multiply2
+    init(1),
+    flow(
+        add1,
+        multiply2,
+        toString
+    )
 )
 
-console.log(out)
+// console.log(out)
+// console.log(typeof out)
 
+
+function concat(a: number, transformer: (a: number) => string): [number, string] {
+  return [a, transformer(a)]
+}
+
+const out1 = concat(1, (n)=> pipe(n, add1, multiply2, toString)) //Плохо
+const out2 = concat(1, flow(add1, multiply2, toString)) //Хорошо
+
+console.log({out1})
+console.log({out2})
