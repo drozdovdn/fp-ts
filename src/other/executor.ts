01d184897a3234255f5ad172215886efcad1aa5c
drozdov <drozdov@skytracking.ru>
2022-07-06 12:55:56 +0300

    fix name

--- src/other/executor.ts
+++ src/other/executor.ts
@@ -4,45 +4,37 @@
 
 type Item<A> = {
   // массив чисел, который будет проверяться на "хоть один находится в"
-  contains: number[],
+  contains: number[];
   // если хоть один находится, то добавить в аккумулятор это значение
-  value: A,
+  value: A;
 };
-/**
- * Проходится по config
- * если в ids есть хотя бы один id из contains то
- * добавляет value в start, возвращая обновленный start
- * @param p
- */
-const executor = <A extends { id: number }>(p: {
-  // стартовое значение аккумулятора
-  start: Record<number, A>,
-  // список из пунктов конфига
-  config: Item<A>[],
-  // общий массив чисел по которому ты матчишься
-  ids: number[],
-}): Record<number, A> => {
-
- const idsSet = new Set(p.ids)
-
- return p.config.reduce((acc, item) => {
-   for (const key of item.contains) {
-     if(acc[item.value.id]) break;
-     if(idsSet.has(key)){
-       return {...acc, [item.value.id]: item.value}
-     }
-   }
-   return acc
- }, p.start)
-
-};
-
 
 type MenuItem = {
-  id: number,
-  name: string,
-  target: string,
-  icon: string,
+  id: number;
+  name: string;
+  target: string;
+  icon: string;
+};
+
+const executor = <A extends { id: number }>(p: {
+  // стартовое значение аккумулятора
+  start: Record<number, A>;
+  // список из пунктов конфига
+  config: Item<A>[];
+  // общий массив чисел по которому ты матчишься
+  ids: number[];
+}): Record<number, A> => {
+  const idsSet = new Set(p.ids);
+
+  return p.config.reduce((acc, item) => {
+    for (const key of item.contains) {
+      if (acc[item.value.id]) break;
+      if (idsSet.has(key)) {
+        return { ...acc, [item.value.id]: item.value };
+      }
+    }
+    return acc;
+  }, p.start);
 };
 
 const config: Item<MenuItem>[] = [
@@ -75,46 +67,20 @@ const config: Item<MenuItem>[] = [
   },
 ];
 
-
-const initStart:Record<number, MenuItem> = {
+const initStart: Record<number, MenuItem> = {
   0: {
     id: 0,
     target: '',
     name: 'name',
-    icon: ''
-  }
-}
+    icon: '',
+  },
+};
 
 // финальный результат
 const data = executor({
   start: initStart,
   config,
-  ids: [1,2,3,7],
+  ids: [1, 2, 3, 7],
 });
 
-console.log({data})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log({ data });
