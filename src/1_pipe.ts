d6c90704157b4d5c41d115b7ae2acfb4367e5bd5
drozdov <drozdov@skytracking.ru>
2022-07-06 12:40:34 +0300

    add lint and add pretter

--- src/1_pipe.ts
+++ src/1_pipe.ts
@@ -1,37 +1,29 @@
-import { pipe, flow } from 'fp-ts/lib/function'
+import { pipe, flow } from 'fp-ts/lib/function';
 
 const init = (num: number): number => {
-    return num
-}
-const add1 = (num:number):number => {
-    return num + 1
-}
+  return num;
+};
+const add1 = (num: number): number => {
+  return num + 1;
+};
 
-const multiply2 = (num:number):number => {
-    return num * 2
-}
-const toString = (num: number):string => {
-    return `${num}`
-}
-const out = pipe(
-    init(1),
-    flow(
-        add1,
-        multiply2,
-        toString
-    )
-)
+const multiply2 = (num: number): number => {
+  return num * 2;
+};
+const toString = (num: number): string => {
+  return `${num}`;
+};
+const out = pipe(init(1), flow(add1, multiply2, toString));
 
 // console.log(out)
 // console.log(typeof out)
 
-
 function concat(a: number, transformer: (a: number) => string): [number, string] {
-  return [a, transformer(a)]
+  return [a, transformer(a)];
 }
 
-const out1 = concat(1, (n)=> pipe(n, add1, multiply2, toString)) //Плохо
-const out2 = concat(1, flow(add1, multiply2, toString)) //Хорошо
+const out1 = concat(1, (n) => pipe(n, add1, multiply2, toString)); //Плохо
+const out2 = concat(1, flow(add1, multiply2, toString)); //Хорошо
 
-console.log({out1})
-console.log({out2})
+console.log({ out1 });
+console.log({ out2 });
